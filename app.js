be01eb3929f3b46e8a7bf9feeb584a8f53c39a79
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-02-28 13:48:35 +0100

    Init commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries-list');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+	let countryName = $('#country-name').val();
+	 
+}
